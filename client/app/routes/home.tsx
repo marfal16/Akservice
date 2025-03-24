744f481235fb93eec570bd9437852da082b6fb3f
Falanga <maria.falanga@accenture.com>
2025-03-24 15:38:07 +0100

    cancellazione welcome

--- client/app/routes/home.tsx
+++ client/app/routes/home.tsx
@@ -1,5 +1,5 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+/* import type { Route } from "./+types/home";
+// import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,11 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <h1>Benvenuti nella nostra Home Page!</h1>
+      <p>Questa è la pagina principale.</p>
+    </div>
+  );
 }
+*/
