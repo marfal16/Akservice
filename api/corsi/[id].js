3b36f8d8277ac98f4212557ed2ac5d86e7f5f24c
Falanga <mariafalanga00@gmail.com>
2025-08-28 16:29:42 +0200

    vercel fix

--- api/corsi/[id].js
+++ api/corsi/[id].js
@@ -1,5 +1,5 @@
 //const pool = require('../../utils/db');
-/*const pool = require('../utils/db');
+const pool = require('../utils/db');
 
 module.exports = async (req, res) => {
   if (req.method !== 'GET') {
@@ -23,30 +23,4 @@ module.exports = async (req, res) => {
     console.error("Errore nel recupero del corso", err);
     res.status(500).json({ error: "Errore del server" });
   }
-}; */
-
-import pool from "../utils/db.js"; // importa il pool dal db.js in ESM
-
-export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).send("Method Not Allowed");
-  }
-
-  const { id } = req.query;
-
-  try {
-    if (isNaN(id)) {
-      return res.status(400).json({ error: "ID non valido" });
-    }
-
-    const result = await pool.query("SELECT * FROM corsi WHERE id = $1", [parseInt(id)]);
-    if (result.rows.length === 0) {
-      return res.status(404).json({ error: "Corso non trovato" });
-    }
-
-    res.status(200).json(result.rows[0]);
-  } catch (err) {
-    console.error("Errore nel recupero del corso", err);
-    res.status(500).json({ error: "Errore del server" });
-  }
-}
+}; 
--- api/utils/db.js
+++ api/utils/db.js
@@ -1,18 +1,8 @@
-/* const { Pool } = require('pg');
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DB_CONNECTION_STRING,
   ssl: { rejectUnauthorized: false }
 });
 
-module.exports = pool; */
-
-import pkg from "pg";
-const { Pool } = pkg;
-
-const pool = new Pool({
-  connectionString: process.env.DB_CONNECTION_STRING,
-  ssl: { rejectUnauthorized: false },
-});
-
-export default pool;
+module.exports = pool; 
