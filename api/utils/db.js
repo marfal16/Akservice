b79c13156baf07e016fe7c1fd3428553bf6c807b
Falanga <mariafalanga00@gmail.com>
2025-08-28 16:13:30 +0200

    vercel fix

--- api/utils/db.js
+++ api/utils/db.js
@@ -1,8 +1,18 @@
-const { Pool } = require('pg');
+/* const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DB_CONNECTION_STRING,
   ssl: { rejectUnauthorized: false }
 });
 
-module.exports = pool;
+module.exports = pool; */
+
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DB_CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false },
+});
+
+export default pool;
