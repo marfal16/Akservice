3a92a9be50aa1a5b69179f059505468211f97b3a
Falanga <mariafalanga00@gmail.com>
2025-08-29 16:02:43 +0200

    fix rec corsi

--- api/corsi/[id].js
+++ api/corsi/[id].js
@@ -1,4 +1,4 @@
-const pool = require('../../utils/db');
+/*const pool = require('../../utils/db');
 //const pool = require('../utils/db');
 
 module.exports = async (req, res) => {
@@ -23,4 +23,30 @@ module.exports = async (req, res) => {
     console.error("Errore nel recupero del corso", err);
     res.status(500).json({ error: "Errore del server" });
   }
-}; 
+}; */
+
+import pool from '../../utils/db';
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).send('Method Not Allowed');
+  }
+
+  const { id } = req.query;
+
+  try {
+    if (isNaN(id)) {
+      return res.status(400).json({ error: "ID non valido" });
+    }
+
+    const result = await pool.query('SELECT * FROM corsi WHERE id = $1', [parseInt(id)]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Corso non trovato" });
+    }
+
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.error("Errore nel recupero del corso", err);
+    res.status(500).json({ error: "Errore del server" });
+  }
+}
