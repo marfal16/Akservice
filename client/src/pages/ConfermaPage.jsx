7abb155594084b038d9eebeec02cc9a91896b3f7
Falanga <mariafalanga00@gmail.com>
2025-09-01 11:30:27 +0200

    refresh carrello

--- client/src/pages/ConfermaPage.jsx
+++ client/src/pages/ConfermaPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import "./ConfermaPage.css";
 
 export default function ConfermaPage() {
@@ -9,4 +9,22 @@ export default function ConfermaPage() {
       <p>Riceverai una email con i dettagli.</p>
     </div>
   );
+}*/
+
+import React, { useEffect } from "react";
+import "./ConfermaPage.css";
+
+export default function ConfermaPage() {
+  useEffect(() => {
+    localStorage.removeItem('cart'); // Svuota il carrello salvato
+  }, []);
+
+  return (
+    <div className="conferma-container">
+      <h1>Grazie per il tuo acquisto!</h1>
+      <p>Il tuo ordine è stato completato con successo.</p>
+      <p>Riceverai una email con i dettagli.</p>
+    </div>
+  );
 }
+
